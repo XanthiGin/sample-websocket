5e0a275b0f2fc063e2be70fd2717a74b805150c7
XanthiGin <157917504+XanthiGin@users.noreply.github.com>
2024-04-12 12:48:23 +0300

    Initial Websocket App

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const { createServer } = require('http');
+
+const WebSocket = require('ws');
+
+const app = express();
+app.use(express.static(__dirname));
+
+const server = createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function (ws) {
+  const id = setInterval(function () {
+    ws.send(JSON.stringify(process.memoryUsage()), function () {
+      //
+      // Ignoring errors.
+      //
+    });
+  }, 100);
+  console.log('started client interval');
+
+  ws.on('close', function () {
+    console.log('stopping client interval');
+    clearInterval(id);
+  });
+});
+
+server.listen(8080, function () {
+  console.log('Listening on http://0.0.0.0:8080');
+});
+
+// Serve a simple HTML page when visiting http://0.0.0.0:8080
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
